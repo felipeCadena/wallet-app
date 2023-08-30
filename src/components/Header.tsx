9442f04a14330bfbfcf2599e6cc03e78a4842cc2
felipeCadena <felipecaden@gmail.com>
2023-08-30 16:59:41 -0300

    req 2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,25 @@
+import { useSelector } from 'react-redux';
+import { ReduxState } from '../types';
+
 function Header() {
-  return <div>Header</div>;
+  const { user } = useSelector((state: ReduxState) => state);
+
+  return (
+    <div>
+      <img src="" alt="" />
+      <p data-testid="email-field">
+        E-mail:
+        {' '}
+        {user.email}
+      </p>
+      <div data-testid="total-field">
+        Despesa Total: 0
+        {' '}
+        {' '}
+        <span data-testid="header-currency-field">BRL</span>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,21 @@
 import { ThunkDispatch } from 'redux-thunk';
 import { AnyAction } from 'redux';
 
+export type UserState = {
+  email: string;
+  password: string;
+};
+
+export type WalletState = {
+  currencies: string[],
+  expenses: object[],
+  editor: boolean;
+  idToEdit: number;
+};
+
 export type ReduxState = {
-  user: {
-    email: string;
-  },
-  wallet: {
-    currencies: string[],
-    expenses: object[],
-    editor: boolean;
-    idToEdit: number;
-  },
+  user: UserState,
+  wallet: WalletState,
 };
 
 export type Dispatch = ThunkDispatch<ReduxState, null, AnyAction>;
